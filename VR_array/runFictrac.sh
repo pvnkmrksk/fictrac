5afeb51138a27210079814efdc733eed124e9075
Pavan Kaushik <pkasuhik@ab.mpg.de>
2023-07-12 13:18:23 +0200

    add bash args to run just the vr number given. if nothing given, run all

--- VR_array/runFictrac.sh
+++ VR_array/runFictrac.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-dir1="$HOME/src/fictrac/VR_array/VR1"
-dir2="$HOME/src/fictrac/VR_array/VR2"
-dir3="$HOME/src/fictrac/VR_array/VR3"
-dir4="$HOME/src/fictrac/VR_array/VR4"
+# Maximum VR number
+max_vr=4
+
+# Check if an argument is provided
+if [ -z "$1" ]
+then
+  # If no argument is provided, run all VRs
+  vr_list=$(seq 1 $max_vr)
+else
+  # If the argument is a valid number (1-max_vr), use it as the VR number
+  if [ "$1" -ge 1 ] && [ "$1" -le $max_vr ]
+  then
+    vr_list=$1
+  else
+    echo "Invalid VR number. Please provide a number between 1 and $max_vr, or no number to run all VRs."
+    exit 1
+  fi
+fi
 
 start_fictrac="sudo ../../bin/fictrac config.txt"
 
-gnome-terminal --tab -- bash -c "cd $dir1; $start_fictrac; exec bash"
-gnome-terminal --tab -- bash -c "cd $dir2; $start_fictrac; exec bash"
-gnome-terminal --tab -- bash -c "cd $dir3; $start_fictrac; exec bash"
-gnome-terminal --tab -- bash -c "cd $dir4; $start_fictrac; exec bash"
-
+# Start the fictrac in the corresponding dirs
+for vr in $vr_list
+do
+  dir="$HOME/src/fictrac/VR_array/VR$vr"
+  gnome-terminal --tab -- bash -c "cd $dir; $start_fictrac; exec bash"
+done
--- VR_array/runSocket.sh
+++ VR_array/runSocket.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Define ZMQ URL ports
-zmq_url_port1="9871"
-zmq_url_port2="9872"
-zmq_url_port3="9873"
-zmq_url_port4="9874"
+# Maximum VR number
+max_vr=4
 
-# Define socket ports
-sock_port1="1111"
-sock_port2="1112"
-sock_port3="1113"
-sock_port4="1114"
+# Check if an argument is provided
+if [ -z "$1" ]
+then
+  # If no argument is provided, run all VRs
+  vr_list=$(seq 1 $max_vr)
+else
+  # If the argument is a valid number (1-max_vr), use it as the VR number
+  if [ "$1" -ge 1 ] && [ "$1" -le $max_vr ]
+  then
+    vr_list=$1
+  else
+    echo "Invalid VR number. Please provide a number between 1 and $max_vr, or no number to run all VRs."
+    exit 1
+  fi
+fi
 
 # Define socket host
 sock_host="127.0.0.1"
@@ -18,9 +25,13 @@ sock_host="127.0.0.1"
 # Define socket publish python file path
 republisher_path="$HOME/src/fictrac/VR_array/socket_zmq_republisher.py"
 
-# Execute the Python script in new tabs
-gnome-terminal --tab -- bash -c "python $republisher_path --zmq-url tcp://*:$zmq_url_port1 --sock-host $sock_host --sock-port $sock_port1; exec bash"
-gnome-terminal --tab -- bash -c "python $republisher_path --zmq-url tcp://*:$zmq_url_port2 --sock-host $sock_host --sock-port $sock_port2; exec bash"
-gnome-terminal --tab -- bash -c "python $republisher_path --zmq-url tcp://*:$zmq_url_port3 --sock-host $sock_host --sock-port $sock_port3; exec bash"
-gnome-terminal --tab -- bash -c "python $republisher_path --zmq-url tcp://*:$zmq_url_port4 --sock-host $sock_host --sock-port $sock_port4; exec bash"
+# Start the Python script in the corresponding VRs
+for vr in $vr_list
+do
+  # Define ZMQ URL port and socket port based on the VR number
+  zmq_url_port="987$(printf %01d $vr)"
+  sock_port="111$(printf %01d $vr)"
 
+  # Execute the Python script in a new tab
+  gnome-terminal --tab -- bash -c "python $republisher_path --zmq-url tcp://*:$zmq_url_port --sock-host $sock_host --sock-port $sock_port; exec bash"
+done
