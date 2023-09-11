d831a2bc3abc15d4c5929217282589cd28dcd1f8
Pavan Kumar Kaushik <pkaushik@ab.mpg.de>
2023-09-11 17:18:09 +0200

    pretty logs for rsoc

--- VR_array/runSocket.sh
+++ VR_array/runSocket.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
 
-# alias rsoc='bash $HOME/src/fictrac/VR_array/runSocket.sh'
+# This script launches socket republishers for VRs in separate tabs.
+# If provided with a number, it will run only the specified VR. Otherwise, it will run all VRs up to the maximum number.
+# Each VR republisher is run in its separate gnome-terminal tab.
 
-# Maximum VR number
-max_vr=4
+# Define constants
+MAX_VR=4
+SOCK_HOST="127.0.0.1"
+REPUBLISHER_PATH="$HOME/src/fictrac/VR_array/socket_zmq_republisher.py"
 
-# Check if an argument is provided
-if [ -z "$1" ]
-then
-  # If no argument is provided, run all VRs
-  vr_list=$(seq 1 $max_vr)
+# Check for provided arguments
+if [ -z "$1" ]; then
+  # No argument: run all VRs
+  VR_LIST=$(seq 1 $MAX_VR)
+elif [[ "$1" =~ ^[0-9]+$ ]] && [ "$1" -ge 1 ] && [ "$1" -le $MAX_VR ]; then
+  # Valid VR number provided: run only the specified VR
+  VR_LIST=$1
 else
-  # If the argument is a valid number (1-max_vr), use it as the VR number
-  if [ "$1" -ge 1 ] && [ "$1" -le $max_vr ]
-  then
-    vr_list=$1
-  else
-    echo "Invalid VR number. Please provide a number between 1 and $max_vr, or no number to run all VRs."
-    exit 1
-  fi
+  # Invalid argument
+  echo "Invalid VR number. Please provide a number between 1 and $MAX_VR, or no number to run all VRs."
+  exit 1
 fi
 
-# Define socket host
-sock_host="127.0.0.1"
+# Loop through the list of VRs and launch each in a separate tab
+for vr in $VR_LIST; do
+  ZMQ_URL_PORT="987$(printf %01d $vr)"
+  SOCK_PORT="111$(printf %01d $vr)"
 
-# Define socket publish python file path
-republisher_path="$HOME/src/fictrac/VR_array/socket_zmq_republisher.py"
+  # Logging the action
+  echo "Launching republisher for VR$vr on zmq-url tcp://*:$ZMQ_URL_PORT and socket $SOCK_HOST:$SOCK_PORT"
 
-# Start the Python script in the corresponding VRs
-for vr in $vr_list
-do
-  # Define ZMQ URL port and socket port based on the VR number
-  zmq_url_port="987$(printf %01d $vr)"
-  sock_port="111$(printf %01d $vr)"
-
-  # Execute the Python script in a new tab
-  gnome-terminal --tab -- bash -c "python $republisher_path --zmq-url tcp://*:$zmq_url_port --sock-host $sock_host --sock-port $sock_port; exec bash"
+  # Execute the Python script in a new gnome-terminal tab
+  gnome-terminal --tab -- bash -c "python $REPUBLISHER_PATH --zmq-url tcp://*:$ZMQ_URL_PORT --sock-host $SOCK_HOST --sock-port $SOCK_PORT; exec bash"
 done
+
--- VR_array/socket_zmq_republisher.py
+++ VR_array/socket_zmq_republisher.py
@@ -7,23 +7,22 @@ import json
 import math
 import click
 
-# Define a function to process one line of data from FicTrac
 def process_line(line, socket_zmq):
-    # Tokenise
+    """
+    Process one line of data from FicTrac.
+    Extract FicTrac variables and send the data to ZMQ.
+    """
     toks = line.split(", ")
 
-    # Check that we have sensible tokens
-    if ((len(toks) < 24) | (toks[0] != "FT")):
-        print('Bad read')
+    # Check the data format.
+    if len(toks) < 24 or toks[0] != "FT":
+        print('Bad read: Unexpected data format.')
         return
 
-    # Extract FicTrac variables
-    # (see https://github.com/rjdmoore/fictrac/blob/master/doc/data_header.txt for descriptions)
     x = float(toks[15])
     y = float(toks[16])
     yaw = float(toks[17])
 
-    # Do something ...
     data_zmq = {
         "x": x,
         "y": y,
@@ -32,62 +31,51 @@ def process_line(line, socket_zmq):
         "pitch": 0.0,
         "roll": 0.0
     }
-    socket_zmq.send_string(json.dumps(data_zmq))
+    try:
+        socket_zmq.send_string(json.dumps(data_zmq))
+    except Exception as e:
+        print(f"Failed to send data over ZMQ. Error: {e}")
 
-# Define the main command using click decorators
 @click.command()
-@click.option('--zmq-url', type=str, default='tcp://*:9871',
-              help='The URL to bind the ZMQ socket to.')
-@click.option('--sock-host', type=str, default='127.0.0.1',
-              help='The host IP address to bind the UDP socket to.')
-@click.option('--sock-port', type=int, default=1111,
-              help='The host port to bind the UDP socket to.')
-@click.option('--timeout', type=int, default=1,
-              help='The timeout in seconds for receiving data from FicTrac.')
+@click.option('--zmq-url', type=str, default='tcp://*:9871', help='ZMQ binding URL.')
+@click.option('--sock-host', type=str, default='127.0.0.1', help='UDP socket host IP.')
+@click.option('--sock-port', type=int, default=1111, help='UDP socket port.')
+@click.option('--timeout', type=int, default=1, help='Timeout for receiving data from FicTrac.')
 def main(zmq_url, sock_host, sock_port, timeout):
-    """A script to receive data from FicTrac and send it to ZMQ."""
-    
-    # Create a ZMQ context and socket
+    """Receive data from FicTrac and transmit it to ZMQ."""
+
+    print(f"Initializing ZMQ binding on {zmq_url}...")
     context = zmq.Context()
     socket_zmq = context.socket(zmq.PUB)
     socket_zmq.bind(zmq_url)
-
-    # Create a UDP socket and bind it to the host and port
+    
+    print(f"Binding UDP socket on {sock_host}:{sock_port}...")
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.bind((sock_host, sock_port))
         sock.setblocking(0)
 
-        # Keep receiving data until FicTrac closes
         data = ""
+        print("Listening for data...")
 
         while True:
-            # Check to see whether there is data waiting
             ready = select.select([sock], [], [], timeout)
-
-            # Only try to receive data if there is data waiting
             if ready[0]:
-                # Receive one data frame
-                new_data = sock.recv(1024)
+                try:
+                    new_data = sock.recv(1024)
+                    if not new_data:
+                        print("No data received. Terminating...")
+                        break
 
-                # Uh oh?
-                if not new_data:
-                    break
-
-                # Decode received data
-                data += new_data.decode('UTF-8')
-
-                # Find the first frame of data
-                endline = data.find("\n")
-                line = data[:endline]       # copy first frame
-                data = data[endline+1:]     # delete first frame
-
-                # Process the line of data
-                process_line(line, socket_zmq)
-
+                    data += new_data.decode('UTF-8')
+                    endline = data.find("\n")
+                    line = data[:endline]
+                    data = data[endline+1:]
+                    
+                    process_line(line, socket_zmq)
+                except Exception as e:
+                    print(f"Error processing data: {e}")
             else:
-                # Didn't find any data - try again
-                print('retrying...')
+                print('No data received within the timeout period. Retrying...')
 
-# Run the main command if this is the main script
 if __name__ == '__main__':
     main()
